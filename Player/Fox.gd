5de09a9488fd127e060603aa56805acece1998f0
jaeycs <jaeycs@gmail.com>
2021-08-27 23:50:23 -0400

    added controls & background

--- Player/Fox.gd
+++ Player/Fox.gd
@@ -1,24 +1,32 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10
+const ACCELERATION = 500
 const MAX_SPEED = 100
-const FRICTION = 10
+const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
-func _physics_process(frameRate):
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector =  input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCERATION * frameRate
-		velcoity = velocity.clamped(MAX_SPEED * frameRate)
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		print(input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * frameRate)
-	
-	move_and_collide(velocity)
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
