80d0bd8b3e52b33108cc4218756be22f00c61eb1
jaeycs <jaeycs@gmail.com>
2021-08-28 15:29:17 -0400

    heath -> health

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -10,7 +10,7 @@ const FRICTION = 200
 const KNOCKBACK_MULTIPLIER = 100
 
 func _ready():
-	print("bat heath: %d/%d" % [stats.health, stats.max_health])
+	print("bat health: %d/%d" % [stats.health, stats.max_health])
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -19,7 +19,7 @@ func _physics_process(delta):
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * KNOCKBACK_MULTIPLIER
-	print("bat heath: %d/%d" % [stats.health, stats.max_health])
+	print("bat health: %d/%d" % [stats.health, stats.max_health])
 
 
 func _on_Stats_no_heath():
