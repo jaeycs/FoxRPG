9fd6d2d1471d7624935b6a295126f133d9c43236
jaeycs <jaeycs@gmail.com>
2021-08-28 15:35:59 -0400

    heath -> health 2

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -9,9 +9,6 @@ var knockback = Vector2.ZERO
 const FRICTION = 200
 const KNOCKBACK_MULTIPLIER = 100
 
-func _ready():
-	print("bat health: %d/%d" % [stats.health, stats.max_health])
-
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
@@ -19,11 +16,8 @@ func _physics_process(delta):
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * KNOCKBACK_MULTIPLIER
-	print("bat health: %d/%d" % [stats.health, stats.max_health])
 
-
-func _on_Stats_no_heath():
-	print("bat dead: %d/%d" % [stats.health, stats.max_health])
+func _on_Stats_no_health():
 	create_death_effect()
 	queue_free()
 
--- Stats.gd
+++ Stats.gd
@@ -3,9 +3,10 @@ extends Node
 export(int) var max_health = 1
 onready var health = max_health setget set_health
 
-signal no_heath
+signal no_health
 
 func set_health(value):
-	health = value
+	health = value	
+	print("bat: %d/%d" % [health, max_health])
 	if health <= 0:
-		emit_signal("no_heath")
+		emit_signal("no_health")
