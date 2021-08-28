334c2f68548d436c2c76429221585e4c495f247b
jaeycs <jaeycs@gmail.com>
2021-08-28 02:16:20 -0400

    add attack

--- Player/Fox.gd
+++ Player/Fox.gd
@@ -4,13 +4,42 @@ const ACCELERATION = 500
 const MAX_SPEED = 100
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-func _physics_process(delta):
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animationTree.active = true
+	print ("Have Fun!")
+
+#func _physics_process(delta):
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func attack_state(delta):
+	animationState.travel("Attack")
+	velocity = Vector2.ZERO
+	
+func attack_animation_finished():
+	state = MOVE
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,6 +48,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		#print(input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -27,10 +57,10 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print ("Have Fun!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
