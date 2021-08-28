595c13fa24c6620d9b9c922874d0f9e368af9d37
jaeycs <jaeycs@gmail.com>
2021-08-28 15:10:54 -0400

    enemy death effect

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
 onready var stats = $Stats
 
 var knockback = Vector2.ZERO
@@ -15,11 +17,17 @@ func _physics_process(delta):
 	knockback = move_and_slide(knockback)
 
 func _on_HurtBox_area_entered(area):
-	stats.health -= 1
+	stats.health -= area.damage
 	knockback = area.knockback_vector * KNOCKBACK_MULTIPLIER
 	print("bat heath: %d/%d" % [stats.health, stats.max_health])
 
 
 func _on_Stats_no_heath():
 	print("bat dead: %d/%d" % [stats.health, stats.max_health])
+	create_death_effect()
 	queue_free()
+
+func create_death_effect():
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
--- Player/SwordHitBox.gd
+++ Player/SwordHitBox.gd
@@ -1,3 +1,3 @@
-extends Area2D
+extends "res://HitBoxes and HurtBoxes/HitBox.gd"
 
 var knockback_vector = Vector2.ZERO
